b0feef6c5213c41dd55aeaacdfbadf774d5acbcd
Samuel Jubault <samuel.jubault@ensg.eu>
2023-01-12 11:40:58 +0100

    03 manque deux tests

--- src/main/java/org/acme/geometry/Geometry.java
+++ src/main/java/org/acme/geometry/Geometry.java
@@ -1,6 +1,9 @@
 package org.acme.geometry;
 
+import sun.awt.X11.XTranslateCoordinates;
+
 public interface Geometry {
     public String getType();
     public Boolean isEmpty();
+
 }
--- src/test/java/org/acme/geometry/PointTest.java
+++ src/test/java/org/acme/geometry/PointTest.java
@@ -7,17 +7,17 @@ public class PointTest {
     public static final double EPSILON = 1.0e-15;
 
     @Test
-    public void testDefaultConstructor(){
-        // TODO
-        Coordinate c = new Coordinate();
-        Assert.assertEquals(0.0, c.getX(), EPSILON);
-        Assert.assertEquals(0.0, c.getY(), EPSILON);
-    }
-    @Test
     public void testValue(){
         // TODO
         Coordinate c = new Coordinate();
         Assert.assertEquals(3.0, c.getX(), EPSILON);
         Assert.assertEquals(4.0, c.getY(), EPSILON);
+    }
+    @Test
+    public void TestisEmpty(){
+        // TODO
+        Point d= new Point();
+        Assert.assertTrue(d.isEmpty());
+        Assert.assertFalse(d.isEmpty());
     }
 }
