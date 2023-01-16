a46148f896e19409b01caf2b94fb284dcd4f650b
Samuel Jubault <samuel.jubault@ensg.eu>
2023-01-16 04:30:53 +0100

    06 rajout class envellopebuider sans test

--- src/main/java/org/acme/geometry/EnvellopeBuilder.java
+++ src/main/java/org/acme/geometry/EnvellopeBuilder.java
@@ -1,17 +1,27 @@
 package org.acme.geometry;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
 public class EnvellopeBuilder {
+
+    private List<Double> xVals = new ArrayList<Double>();
+    private List<Double> yVals = new ArrayList<Double>();
+
 
     public EnvellopeBuilder(){
 
     }
     public void insert(Coordinate coordinate){
-
+        xVals.add(coordinate.getX());
+        yVals.add(coordinate.getY());
     }
 
     public  Envelloppe build(){
-
-
+        Coordinate botomleft= new Coordinate(Collection.min(xVals),Collection.min(yVals));
+        Coordinate topright= new Coordinate(Collection.max(xVals),Collection.max(yVals));
+        return new Envelloppe(botomleft,topright);
     }
 
 
