5a0e62f5504764d94917aeac0a4a736605385f7d
Samuel Jubault <samuel.jubault@ensg.eu>
2023-01-16 03:43:10 +0100

    06 rajout class envellopebuider

---
+++ src/main/java/org/acme/geometry/EnvellopeBuilder.java
@@ -0,0 +1,18 @@
+package org.acme.geometry;
+
+public class EnvellopeBuilder {
+
+    public EnvellopeBuilder(){
+
+    }
+    public void insert(Coordinate coordinate){
+
+    }
+
+    public  Envelloppe build(){
+
+
+    }
+
+
+}
